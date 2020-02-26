72c7566e4a53f147db1cea9986aa554647da0309
Kai Peterson <pete9372@umn.edu>
2020-02-25 22:31:00 -0600

    fix business logic for making calculations (following order of operations)

--- src/main/java/com/example/dmdashboard/CalculateResult.java
+++ src/main/java/com/example/dmdashboard/CalculateResult.java
@@ -1,53 +1,55 @@
 package com.example.dmdashboard;
 
+import java.util.ArrayList;
+
 public class CalculateResult {
 
     public static double calculateResult(String equation) {
 
-//        ArrayList<String> arrayList = new ArrayList<>();
-//
-//        for(int i = 0; i < equation.split(" ").length; i++) {
-//            arrayList.add(equation.split(" ")[i]);
-//        }
-//
-//        while(arrayList.contains("*") || arrayList.contains("/")) {
-//            for (int i = 0; i < arrayList.size(); i++) {
-//                if (arrayList.get(i) == "*") {
-//                    int tempResult = Integer.parseInt(arrayList.get(i - 1)) * Integer.parseInt(arrayList.get(i + 1));
-//                    arrayList.set(i, Integer.toString(tempResult));
-//                    arrayList.remove(i - 1);
-//                    arrayList.remove(i + 1);
-//                    i--;
-//                } else if (arrayList.get(i) == "/") {
-//                    int tempResult = Integer.parseInt(arrayList.get(i - 1)) / Integer.parseInt(arrayList.get(i + 1));
-//                    arrayList.set(i, Integer.toString(tempResult));
-//                    arrayList.remove(i - 1);
-//                    arrayList.remove(i + 1);
-//                    i--;
-//                }
-//            }
-//        }
-//
-//        while(arrayList.contains("+") || arrayList.contains("/")) {
-//            for (int i = 0; i < arrayList.size(); i++) {
-//                if (arrayList.get(i) == "+") {
-//                    int tempResult = Integer.parseInt(arrayList.get(i - 1)) + Integer.parseInt(arrayList.get(i + 1));
-//                    arrayList.set(i, Integer.toString(tempResult));
-//                    arrayList.remove(i - 1);
-//                    arrayList.remove(i + 1);
-//                    i--;
-//                } else if (arrayList.get(i) == "-") {
-//                    int tempResult = Integer.parseInt(arrayList.get(i - 1)) - Integer.parseInt(arrayList.get(i + 1));
-//                    arrayList.set(i, Integer.toString(tempResult));
-//                    arrayList.remove(i - 1);
-//                    arrayList.remove(i + 1);
-//                    i--;
-//                }
-//            }
-//        }
+        ArrayList<String> arrayList = new ArrayList<>();
 
-//        return Double.parseDouble(arrayList.get(0));
-        return 1.0;
+        for(int i = 0; i < equation.split(" ").length; i++) {
+            arrayList.add(equation.split(" ")[i]);
+        }
+
+        while(arrayList.contains("*") || arrayList.contains("/")) {
+            for (int i = 0; i < arrayList.size(); i++) {
+                if (arrayList.get(i).equals("*")) {
+                    double tempResult = Double.parseDouble(arrayList.get(i - 1)) * Double.parseDouble(arrayList.get(i + 1));
+                    arrayList.set(i, Double.toString(tempResult));
+                    arrayList.remove(i - 1);
+                    arrayList.remove(i);
+                    i--;
+                } else if (arrayList.get(i).equals("/")) {
+                    double tempResult = Double.parseDouble(arrayList.get(i - 1)) / Double.parseDouble(arrayList.get(i + 1));
+                    arrayList.set(i, Double.toString(tempResult));
+                    arrayList.remove(i - 1);
+                    arrayList.remove(i);
+                    i--;
+                }
+            }
+        }
+
+        while(arrayList.contains("+") || arrayList.contains("-")) {
+            for (int i = 0; i < arrayList.size(); i++) {
+                if (arrayList.get(i).equals("+")) {
+                    double tempResult = Double.parseDouble(arrayList.get(i - 1)) + Double.parseDouble(arrayList.get(i + 1));
+                    arrayList.set(i, Double.toString(tempResult));
+                    arrayList.remove(i - 1);
+                    arrayList.remove(i);
+                    i--;
+                } else if (arrayList.get(i).equals("-")) {
+                    double tempResult = Double.parseDouble(arrayList.get(i - 1)) - Double.parseDouble(arrayList.get(i + 1));
+                    arrayList.set(i, Double.toString(tempResult));
+                    arrayList.remove(i - 1);
+                    arrayList.remove(i);
+                    i--;
+                }
+            }
+        }
+
+        return Double.parseDouble(arrayList.get(0));
+
     }
 
 }
