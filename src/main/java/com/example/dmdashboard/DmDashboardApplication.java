0ebbce5746c3b4e7846130c865c023d322fc35ad
Kai Peterson <pete9372@umn.edu>
2020-02-27 18:32:01 -0600

    configure CORS to accept requests from localhost:3000 (port for dm-dashboard-react)

--- src/main/java/com/example/dmdashboard/DmDashboardApplication.java
+++ src/main/java/com/example/dmdashboard/DmDashboardApplication.java
@@ -2,12 +2,27 @@ package com.example.dmdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DmDashboardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DmDashboardApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/rolls").allowedOrigins("http://localhost:3000");
+                registry.addMapping("/calculations").allowedOrigins("http://localhost:3000");
+                registry.addMapping("/audioclips").allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 
 }
